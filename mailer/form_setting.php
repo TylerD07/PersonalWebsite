91af15d9359aea7b9d50b978cab04df6abe5c00f
TylerD07 <tyler.daughtry@aol.com>
2019-01-28 02:23:08 -0500

    going to bed if fixing errors with headers doesnt work

--- mailer/form_setting.php
+++ mailer/form_setting.php
@@ -5,4 +5,7 @@
 	$from = 'MailFromPortfolio713@aol.com'; // Admin e-mail
 	$fromName = 'Daughtry Inc.'; // Your company name
 	$charset = 'UTF-8';
+	$header = 'Access-Control-Allow-Origin: *';
+	$header = 'Access-Control-Allow-Methods: GET, POST';
+	$header = 'Access-Control-Allow-Headers: X-Requested-With';
 ?>
